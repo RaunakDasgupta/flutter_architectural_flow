2e1561f60ee6cbbf6ae5420073b57942002dedad
Sitam Sardar <sitamadex11@gmail.com>
2024-12-12 14:26:35 +0530

    Basic Project with first commit

---
+++ android/app/src/main/kotlin/com/heysitam/flutter_architectural_flow/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.heysitam.flutter_architectural_flow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/view/my_home_page.dart
@@ -0,0 +1,55 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ValueNotifier<int> _counter = ValueNotifier(0);
+
+  void _incrementCounter() {
+    _counter.value = _counter.value + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            ValueListenableBuilder(
+              valueListenable: _counter,
+              builder: (context, value, widget) {
+                log("Is Notifying $value");
+                return Text(
+                  '$value',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              }
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
